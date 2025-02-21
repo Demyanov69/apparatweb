7a2f5d2fa6e6770aefad84d8e13f1da7574350d4
Demyanov69 <kukusyvika@gmail.com>
2025-02-22 00:06:11 +0300

    Update index.php

--- 3/index.php
+++ 3/index.php
@@ -5,10 +5,9 @@ header('Content-Type: text/html; charset=UTF-8');
 
 // Конфигурация базы данных
 $db_host = 'localhost';
-$db_name = 'u68761'; // Замените на ваше имя базы данных
-$db_user = 'u68761';   // Замените на ваш логин
-$db_pass = '7216447';   // Замените на ваш пароль
-
+$db_name = 'u68761'; 
+$db_user = 'u68761';   
+$db_pass = '7216447';   
 try {
     $db = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_pass,
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_PERSISTENT => true]);
